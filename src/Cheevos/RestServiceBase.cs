26ecb18e1eef9f8582167a8064ac3ccffed60660
chris <lancer1977@gmail.com>
2024-12-31 03:31:14 -0500

    cheevos

--- src/Cheevos/RestServiceBase.cs
+++ src/Cheevos/RestServiceBase.cs
@@ -1,4 +1,10 @@
 namespace PolyhydraGames.RACheevos;
+
+public static class Urls
+{
+    public static string SystemImagePrefix_32x = "https://static.retroachievements.org/assets/images/system/32x.png";
+    public static string GameLink = "https://retroachievements.org/game/";
+}
 public class CustomDateFormatConverter : System.Text.Json.Serialization.JsonConverter<DateTime>
 {
     private readonly string dateFormat = "yyyy-MM-dd HH:mm:ss";
